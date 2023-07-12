879fe8def3dc74f2c1c92c5bb69964ff63688fd2
Giorgi Vashakidze <gvashakidze@kaltura.com>
2023-07-12 12:13:15 +0400

    ...

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "identomat-iOS",
+    defaultLocalization: "ka",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
